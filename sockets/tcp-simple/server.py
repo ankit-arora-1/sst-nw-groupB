ecfc356db71331530b0e274134c6bff0f6900dee
Ankit Arora <arora.ankit7@gmail.com>
2025-09-02 10:59:44 +0530

    Added simple tcp cpde

---
+++ sockets/tcp-simple/server.py
@@ -0,0 +1,21 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+
+interface = "127.0.0.1"
+port = 12345
+sock.bind((interface, port))
+print(f"Socket bound to the port: {port}")
+
+sock.listen()
+print("Socket is listening")
+
+conn, addr = sock.accept()
+print(f"Got connection from client: {addr}")
+
+data = conn.recv(1024)
+print(f"Data from client = {data.decode()}")
+
+conn.close()
+sock.close()
